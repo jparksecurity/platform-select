c00303a95dd044f6e8a788d1a0cd7db68df1b202
John Park <jpark@trelly.com>
2024-03-21 16:25:38 -0300

    Add platform select

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! {Platform.select({native: 'native', default: 'default'})}</Text>
       <StatusBar style="auto" />
     </View>
   );
